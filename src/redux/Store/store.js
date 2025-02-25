883468777c40ff999e0779c1423f05d4d9ce1c88
Roshniparasharr <139575123+Roshniparasharr@users.noreply.github.com>
2025-02-25 11:45:45 +0530

    Admin bug fix

--- src/redux/Store/store.js
+++ src/redux/Store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../User/mainSlice';
 import organizationReducer from '../Organization/organizationSlice';
 import etfReducer from '../Common/etfSlice';
+import adminReducer from '../Admin/AdminSlice';   
 
 const store = configureStore({
   reducer: {
     user: userReducer,  // Single reducer for all user-related slices
     organization: organizationReducer,
     stock: etfReducer,
+    admin: adminReducer,
   },
 });
 
