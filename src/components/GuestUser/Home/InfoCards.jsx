57d5c4b4c89d6addf7ad62b380b27a9a9112f2d6
AashiGupta1605 <115386205+AashiGupta1605@users.noreply.github.com>
2025-02-28 23:52:02 +0530

    InfoCards.jsx and OrganizationsSlider.jsx created

--- src/components/GuestUser/Home/InfoCards.jsx
+++ src/components/GuestUser/Home/InfoCards.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useSpring, animated } from "react-spring";
+// import { useSpring, animated } from "react-spring";
 
 const InfoCards = () => {
 
