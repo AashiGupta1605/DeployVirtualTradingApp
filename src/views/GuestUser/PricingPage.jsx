00f1f439b5ac3e877df16cf822c56fd44fd29ef3
AashiGupta1605 <115386205+AashiGupta1605@users.noreply.github.com>
2025-03-09 01:03:37 +0530

    add different pricing and pgr logo in navbar

--- src/views/GuestUser/PricingPage.jsx
+++ src/views/GuestUser/PricingPage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import PricingPageTopMost from "../../components/GuestUser/Pricing/PricingPageTopMost";
-import Pricing from "../../components/GuestUser/Pricing/Pricing";
+// import Pricing from "../../components/GuestUser/Pricing/Pricing";
 // import DummyPricing from '../../components/GuestUser/Pricing/DummyPricing'
 import MainHomeNavbar from "../../components/GuestUser/Navbars/MainHomeNavbar";
+import SubscriptionModal from "../../components/GuestUser/Pricing/SubscriptionModal";
 // import NiftyNavbarCarousel from '../../components/GuestUser/Navbars/NiftyNavbarCarousel'
 // import EtfNavbarCarousel from '../../components/GuestUser/Navbars/EtfNavbarCarousel'
 import Footer from "../../components/GuestUser/Footers/Footer";
@@ -14,13 +15,13 @@ const PricingPage = () => {
       {/* <NiftyNavbarCarousel fixed/> */}
       {/* <EtfNavbarCarousel fixed/> */}
       <div className="mt-12">
-        <div className="bg-gray-50 flex items-center justify-center p-4">
+      <SubscriptionModal/>
+        {/* <div className="bg-gray-50 flex items-center justify-center p-4">
           <div className="max-w-7xl mx-auto px-4 py-16">
             <PricingPageTopMost />
             <Pricing />
-            {/* <DummyPricing/> */}
           </div>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </>
